6fa673e86722150cc00f703539ff4fe6478a45e3
tatchi <tinencor@gmail.com>
2020-04-17 19:12:21 +0200

    add tooltip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import {
   Tooltip,
   ResponsiveContainer,
   Label,
+  TooltipProps,
 } from 'recharts';
 import {
   fetchBandwidth,
@@ -38,6 +39,95 @@ const CustomYAxisTick = (props: any) => {
         )}
       </text>
     </g>
+  );
+};
+
+const CustomTooltip: React.FC<TooltipProps> = ({ active, payload, label }) => {
+  if (active) {
+    return (
+      <div className="recharts-curve recharts-tooltip-cursor">
+        <p className="desc">Anything you want can be displayed here.</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const ContentTooltip: React.FC<TooltipProps> = (props) => {
+  const { active, label = '', payload = [] } = props;
+  if (!active) return null;
+
+  console.log(props);
+
+  const total = Number(payload[0].value) + Number(payload[1].value);
+
+  return (
+    <div
+      style={{
+        backgroundColor: 'white',
+        padding: 8,
+        fontSize: 14,
+        border: '1px solid rgba(0, 0, 0, 0.1)',
+        boxShadow: '0 6px 18px 0 rgba(0, 0, 0, 0.2)',
+      }}
+    >
+      <div style={{ marginBottom: 8, fontWeight: 'bold' }}>
+        {new Date(label).toString().split(' GMT')[0]}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+        }}
+      >
+        {payload.map((data) => (
+          <div key={data.name} style={{ marginTop: 8 }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <span style={{ color: data.color, fontSize: 8, marginRight: 4 }}>
+                ●
+              </span>
+              <span style={{ color: 'grey' }}>{data.name.toUpperCase()}:</span>
+              &nbsp;
+              <span style={{ color: data.color }}>{`${Number(
+                data.value
+              ).toFixed(2)} Gpbs`}</span>
+            </div>
+          </div>
+        ))}
+        <div
+          style={{
+            width: '100%',
+            marginTop: 8,
+            marginBottom: 8,
+          }}
+        >
+          <hr
+            style={{
+              width: '100%',
+              border: 'none',
+              borderTop: '1px solid rgba(0, 0, 0, 0.1)',
+            }}
+          />
+        </div>
+        <div>
+          <span style={{ color: 'grey' }}>Total:</span>{' '}
+          <span style={{ color: 'green' }}>
+            {total.toFixed(2)}
+            &nbsp;Gbps
+          </span>
+        </div>
+        <div style={{marginTop: 8}}>
+          <span style={{ color: 'grey' }}>
+            Spike reduction:
+          </span>{' '}
+          <span style={{ color: 'green' }}>
+            {((Number(payload[0].value) / total) * 100).toFixed(2)}&nbsp;%
+          </span>
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -154,7 +244,7 @@ function App() {
             tick={<CustomizedXAxisTick />}
           />
           <YAxis tick={<CustomYAxisTick />} />
-          <Tooltip />
+          <Tooltip content={<ContentTooltip />} />
           <Area
             type="monotone"
             dataKey="p2p"
@@ -166,6 +256,7 @@ function App() {
           <Area
             type="monotone"
             dataKey="cdn"
+            name="http"
             stackId="1"
             stroke="#B2125C"
             strokeWidth={2}
